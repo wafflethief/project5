b18b9898d98517443f07dacd70d9b8361258d59e
wafflethief <janlouie810@gmail.com>
2021-03-24 09:10:36 -0400

    tts not workin

--- app/src/main/java/com/example/project_5/MainActivity.java
+++ app/src/main/java/com/example/project_5/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.project_5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -27,10 +29,15 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    public Button button_kanye;
+    private Button button_kanye;
+    private TextToSpeech tts;
+    private String quote = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,41 +46,66 @@ public class MainActivity extends AppCompatActivity {
 
         // assign buttons
         button_kanye = (Button) findViewById(R.id.button_kanye);
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR){
+                    int result = tts.setLanguage(Locale.ENGLISH);
+                    if(result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED){
+                        Log.e("TTS", "Not supported");
+                    }
+                    else{
+                        button_kanye.setEnabled(true);
+                    }
+                }
+                else{
+                    Log.e("TTS", "Not supported");
+                }
+            }
+        });
 
         // onClick listeners for each button
         button_kanye.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                tts.speak("heyoo", TextToSpeech.QUEUE_FLUSH, null);
                 // Instantiate the RequestQueue.
                 RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
                 // this url is a json OBJECT
-                //String url ="https://api.met.no/weatherapi/oceanforecast/0.9/.json?lat=80&lon=120";
                 String url = "https://api.kanye.rest";
+
                 JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
                         (Request.Method.GET, url, null, new Response.Listener<JSONObject>()
                 {
                     @Override
                     public void onResponse(JSONObject response) {
-                        String quote = "";
                         try {
                             quote = response.getString("quote");
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
                         Toast.makeText(MainActivity.this, quote, Toast.LENGTH_LONG).show();
-                        //Toast.makeText(MainActivity.this, "Temperature: " + temp, Toast.LENGTH_LONG).show();
+                        //tts.speak(quote, TextToSpeech.QUEUE_FLUSH,null);
+                        //speak(quote);
                     }
                 }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         Toast.makeText(MainActivity.this, "error", Toast.LENGTH_LONG).show();
                     }
+
                 });
                 // Add the request to the RequestQueue.
                 queue.add(jsonObjectRequest);
-
-                //Toast.makeText(getApplicationContext(), "You clicked compliment", Toast.LENGTH_LONG).show();
+                speak(quote);
             }
         });
+    }
+
+    public void speak(String quote){
+        tts.setPitch(0.1f);
+        tts.setSpeechRate(0.1f);
+        tts.speak("margarita", TextToSpeech.QUEUE_FLUSH, null);
     }
 }
